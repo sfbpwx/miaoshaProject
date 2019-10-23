e3e8faec6a3c5d4098f98ac1cc74b69e45e9fdde
124008127@qq.com <Wangxian1989>
2019-10-23 11:15:48 +0800

    aabb

---
+++ src/main/java/com/miaosha/miaoshaProject/MiaoshaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.miaosha.miaoshaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiaoshaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiaoshaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miaosha/miaoshaProject/MiaoshaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miaosha.miaoshaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiaoshaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
