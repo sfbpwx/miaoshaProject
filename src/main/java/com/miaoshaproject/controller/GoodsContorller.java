39837d7a2f34c7b2492fcaf7f910cfef99bb5473
124008127@qq.com <Wangxian1989>
2019-11-19 19:47:47 +0800

    GoodsContorller类的错误纠正

--- src/main/java/com/miaoshaproject/controller/GoodsContorller.java
+++ src/main/java/com/miaoshaproject/controller/GoodsContorller.java
@@ -51,78 +51,82 @@ public class GoodsContorller {
         }
         WebContext webContext = new WebContext(request,response,request.getServletContext(),request.getLocale(),model.asMap());
         html = thymeleafViewResolver.getTemplateEngine().process("goods_list",webContext);
-        if(!StringUtils.isEmpty(html))
-            redisService.set(GoodsKey.getGoodsList,"",html);
-        return  html;
-    }
-
-    @RequestMapping(value = "/to_detail2/{goodsId}",produces = "text/html")
-    @ResponseBody
-    public Result<GoodsDetailVo> toDetail2(MiaoshaUser user, @PathVariable("goodsId")long goodsId, Model model, HttpServletRequest request, HttpServletResponse response){
-        //snowflake
-        GoodsVo goodsVo = goodsService.getGoodsVoByGoodsId(goodsId);
-        long startTime = goodsVo.getStartDate().getTime();
-        long endTime = goodsVo.getEndDate().getTime();
-        long now = System.currentTimeMillis();
-        int miaoshaStatus = 0;
-        int remainSeconds = 0;
-        if(now < startTime ) {//秒杀还没开始，倒计时
-            miaoshaStatus = 0;
-            remainSeconds = (int)((startTime - now )/1000);
-        }else  if(now > endTime){//秒杀已经结束
-            miaoshaStatus = 2;
-            remainSeconds = -1;
-        }else {//秒杀进行中
-            miaoshaStatus = 1;
-            remainSeconds = 0;
-        }
-        model.addAttribute("goods",goodsVo);
-        model.addAttribute("miaoshaStatus",miaoshaStatus);
-        model.addAttribute("remainSeconds",remainSeconds);
-        model.addAttribute("user",user);
-//        return "goods_detail";
-        GoodsDetailVo goodsDetailVo = new GoodsDetailVo();
-        goodsDetailVo.setGoods(goodsVo);
-        goodsDetailVo.setUser(user);
-        goodsDetailVo.setMiaoshaStatus(miaoshaStatus);
-        goodsDetailVo.setRemainSeconds(remainSeconds);
-        return  html;
-    }
-
-    @RequestMapping(value = "/to_detail/{goodsId}",produces = "text/html")
-    @ResponseBody
-    public String toDetail(MiaoshaUser user, @PathVariable("goodsId")long goodsId, Model model,HttpServletRequest request, HttpServletResponse response){
-        //snowflake
-        GoodsVo goodsVo = goodsService.getGoodsVoByGoodsId(goodsId);
-        long startTime = goodsVo.getStartDate().getTime();
-        long endTime = goodsVo.getEndDate().getTime();
-        long now = System.currentTimeMillis();
-        int miaoshaStatus = 0;
-        int remainSeconds = 0;
-        if(now < startTime ) {//秒杀还没开始，倒计时
-            miaoshaStatus = 0;
-            remainSeconds = (int)((startTime - now )/1000);
-        }else  if(now > endTime){//秒杀已经结束
-            miaoshaStatus = 2;
-            remainSeconds = -1;
-        }else {//秒杀进行中
-            miaoshaStatus = 1;
-            remainSeconds = 0;
-        }
-        model.addAttribute("goods",goodsVo);
-        model.addAttribute("miaoshaStatus",miaoshaStatus);
-        model.addAttribute("remainSeconds",remainSeconds);
-        model.addAttribute("user",user);
-//        return "goods_detail";
-        String html = redisService.get(GoodsKey.getGoodsDetail,""+goodsId,String.class);
         if(!StringUtils.isEmpty(html)){
+            redisService.set(GoodsKey.getGoodsList,"",html);
+        }
+
+        return  html;
+    }
+
+    @RequestMapping(value="/to_detail2/{goodsId}",produces="text/html")
+    @ResponseBody
+    public String detail2(HttpServletRequest request, HttpServletResponse response, Model model,MiaoshaUser user,
+                          @PathVariable("goodsId")long goodsId) {
+        model.addAttribute("user", user);
+
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsDetail, ""+goodsId, String.class);
+        if(!org.apache.commons.lang3.StringUtils.isEmpty(html)) {
             return html;
         }
-        WebContext webContext = new WebContext(request,response,request.getServletContext(),request.getLocale(),model.asMap());
-        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail",webContext);
-        if(!StringUtils.isEmpty(html))
-            redisService.set(GoodsKey.getGoodsDetail,""+goodsId,html);
-        return  html;
+        //手动渲染
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        model.addAttribute("goods", goods);
+
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if(now < startAt ) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int)((startAt - now )/1000);
+        }else  if(now > endAt){//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        }else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        model.addAttribute("miaoshaStatus", miaoshaStatus);
+        model.addAttribute("remainSeconds", remainSeconds);
+//        return "goods_detail";
+        WebContext ctx = new WebContext(request,response,
+                request.getServletContext(),request.getLocale(), model.asMap());
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
+        if(!org.apache.commons.lang3.StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, ""+goodsId, html);
+        }
+        return html;
+    }
+
+    @RequestMapping(value="/detail/{goodsId}")
+    @ResponseBody
+    public Result<GoodsDetailVo> detail(HttpServletRequest request, HttpServletResponse response, Model model,MiaoshaUser user,
+                                        @PathVariable("goodsId")long goodsId) {
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        long startAt = goods.getStartDate().getTime();
+        long endAt = goods.getEndDate().getTime();
+        long now = System.currentTimeMillis();
+        int miaoshaStatus = 0;
+        int remainSeconds = 0;
+        if(now < startAt ) {//秒杀还没开始，倒计时
+            miaoshaStatus = 0;
+            remainSeconds = (int)((startAt - now )/1000);
+        }else  if(now > endAt){//秒杀已经结束
+            miaoshaStatus = 2;
+            remainSeconds = -1;
+        }else {//秒杀进行中
+            miaoshaStatus = 1;
+            remainSeconds = 0;
+        }
+        GoodsDetailVo vo = new GoodsDetailVo();
+        vo.setGoods(goods);
+        vo.setUser(user);
+        vo.setRemainSeconds(remainSeconds);
+        vo.setMiaoshaStatus(miaoshaStatus);
+        return Result.success(vo);
     }
 
 }
